f6914591e173e59ac57a5510f57295282192169f
Sire Bah <bahsire1@gmail.com>
2025-06-19 23:55:22 +0000

    destructuration des donnees

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -5,7 +5,7 @@ type User = {
     firstName: string;
     lastName: string;
     gender: string;
-    email:string;
+    email?:string;
     phone: string;
     address:string;
     birthDate: string;
@@ -21,15 +21,15 @@ type Props = {
 
 
 
-function Users(props: Props){
+function Users({users}: Props){ // on peut directement recupéré les users au niveau des paramètres
 
    // const users = props.users;
-    const {users} = props;
+   // const {users} = props; possible également
 
     return(
        <section className="grid md:grid-cols-3 gap-4">
             {
-                users.map(({firstName, lastName, email, phone}: User) =>(
+                users.map(({firstName, lastName, email="INDISPONIBLE", phone}: User) =>(
                     <article className="text-sm border bg-gray-200 border-gray-800 p-1.5 rounded-lg">
                         <h3 className="text-lg">{firstName} {lastName}</h3>
                         <p>{email}</p>
