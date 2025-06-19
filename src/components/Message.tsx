8116486a8d875e79eefce5adaad4285ce77187ce
Sire Bah <bahsire1@gmail.com>
2025-06-19 09:51:28 +0000

    petit exemple exemple

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,13 +2,20 @@ type Personne = {
     prenom : string;
     nom : string;
     email?: string; // Le point d'interrogation indique que cette propriété est optionnelle
+    phone?: string; // Le point d'interrogation indique que cette propriété est optionnelle
 }
 
 
-
+//  le ?. permet de vérifier si email existe avant de l'afficher
+        // <p>Bonjour {personne.prenom} {personne.nom} {personne.email ? <span>{personne.email}</span> : null}</p> //  la condition ternaire permet de vérifier si email existe avant de l'afficher
 function Message(personne: Personne){
     return (
+        <>
         <p>Bonjour {personne.prenom} {personne.nom}</p>
+        <p> {personne?.email && <span> votre email:{personne.email}</span>}</p>
+        <p>{personne?.phone ? <span> et votre téléphone: {personne.phone}</span> : null}</p>
+        </>
+        
     );
 }
 export default Message;
