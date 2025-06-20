a58cb36175ce66472395ac193e62889af1301b43
Sire Bah <bahsire1@gmail.com>
2025-06-20 16:52:16 +0000

    Ajout des buttons pour le trie

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -23,20 +23,34 @@ type Props = {
 
 function Users({users}: Props){ // on peut directement recupéré les users au niveau des paramètres
 
+    const sortUsersBy = (field: string) => {
+        console.log(field)
+
+    };
+
    // const users = props.users;
    // const {users} = props; possible également
 
     return(
-       <section className="grid md:grid-cols-3 gap-4">
+       <section>
+
+            <div className="flex items-center justify-end gap-3 my-3">
+                <span>Trier par...</span>
+                <button type="button" className="border rounded-md border-gray-100 text-white text-sm px-1 py-1 bg-sky-500 hover:bg-sky-700" onClick={()=>sortUsersBy("male")}>Homme</button>
+                <button type="button" className="border rounded-md border-gray-100 text-white text-sm px-1 py-1 bg-sky-500 hover:bg-sky-700" onClick={()=>sortUsersBy("female")}>Femme</button>
+
+            </div>
+            <div className="grid md:grid-cols-3 gap-4">
             {
-                users.map(({firstName, lastName, email="INDISPONIBLE", phone}: User) =>(
-                    <article className="text-sm border bg-gray-200 border-gray-800 p-1.5 rounded-lg">
-                        <h3 className="text-lg">{firstName} {lastName}</h3>
+                users.map(({gender, firstName, lastName, email="INDISPONIBLE", phone, id}: User) =>(
+                    <article className="text-sm border bg-gray-200 border-gray-800 p-1.5 rounded-lg" key={id}>
+                        <h3 className="text-lg">{gender} {firstName} {lastName}</h3>
                         <p>{email}</p>
                         <p>{phone}</p>
                     </article>
                 ) )
             }
+            </div>
        </section>
     )
 
