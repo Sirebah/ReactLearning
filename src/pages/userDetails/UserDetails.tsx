52a82f09a7cda5c51373ebbbb7a1ff65befeb13d
Sire Bah <sire.bah@webbfontaine.com>
2025-06-26 10:03:16 +0000

    commentaire

--- src/pages/userDetails/UserDetails.tsx
+++ src/pages/userDetails/UserDetails.tsx
@@ -25,7 +25,7 @@ function UserDetails() {
     let {userId} = useParams();
     console.log(userId)
     
-
+  // fonction to fetch user details based on userId
     const getOneUser = async () => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
 
